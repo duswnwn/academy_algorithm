66ede07263e4accef1ad1b5dd14a0d26ab3d3f9a
duswnwn <rita27@naver.com>
2023-07-13 11:41:35 +0900

    그래프 알고리즘_BFS

---
+++ programming03_1.js
@@ -0,0 +1,25 @@
+function solution(n, edge) {
+    const graph = Array.from(Array(n + 1), () => []);
+
+    for (const [src, dest] of edge) {
+        graph[src].push(dest);
+        graph[dest].push(src);
+    }
+
+    const distance = Array(n+1).fill(0);
+    distance[1] = 1;
+
+    //BFS
+    const queue = [1];
+    while (queue.length > 0) {
+        const src = queue.shift(); //shift 는 O(n) 이지만 요소가 적을 경우엔 js 엔진에서 최적화를 해줌
+        for (const dest of graph[src]) {
+            if (distance[dest] === 0) {
+                queue.push(dest);
+                distance[dest] = distance[src] + 1
+            }
+        }
+    }
+    const max = Math.max(...distance);
+    return distance.filter(item => item === max).length;
+}
